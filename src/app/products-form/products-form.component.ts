d590d0164cc899a48fc28f9dbbd0b0d11863bd0a
Evgeniy Tilha <evgeniitilga@gmail.com>
2019-04-27 20:46:00 +0300

    adding reactive form

--- src/app/products-form/products-form.component.ts
+++ src/app/products-form/products-form.component.ts
@@ -1,23 +1,35 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Product } from '../models/product.model';
 import { Store } from '@ngrx/store';
 import { AppState } from '../store/app.state';
 import { AddProduct } from '../store/products.action';
+import { Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-products-form',
   templateUrl: './products-form.component.html',
   styleUrls: ['./products-form.component.scss']
 })
-export class ProductsFormComponent {
+export class ProductsFormComponent implements OnInit {
   private id = 2;
   productName = '';
   productModel = '';
 
+  form: FormGroup;
 
   constructor(private store: Store<AppState>) { }
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      productName: new FormControl('', Validators.required),
+      productModel: new FormControl('', Validators.required),
+    });
+  }
+
+
   onAdd() {
+
+    console.log(this.form);
 
     if (this.productName === '' || this.productModel === '' ) {
        return;
